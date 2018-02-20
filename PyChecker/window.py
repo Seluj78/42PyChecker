88517ad6e7ae10fea16846d2b6854d6ca119a562
Jules Lasne <jlasne@student.42.fr>
2018-02-20 23:45:14 +0100

    [FIX] Fixed the error from last commit where the checkboxes wouldn't update

--- PyChecker/window.py
+++ PyChecker/window.py
@@ -26,6 +26,29 @@ class Checkbar(Frame):
 class Application():
     def __init__(self):
         self.window = Tk()
+        self.window.title("42PyChecker")
+        self.window.minsize(width=800, height=800)
+        self.options_dict = {'other': ['author', 'norme', 'Makefile'],
+                              'libft': ['author', 'forbidden-functions', 'makefile', 'norme', 'static', 'extra', 'required', 'bonus', 'benchmark', 'tests', 'libftest', 'maintest', 'moulitest', 'libft-unit-test'],
+                              'fillit': ['author', 'forbidden-functions', 'makefile', 'norme', 'tests', 'fillit-checker'],
+                              '42commandements': []}
+        # @todo: Make groups like when you tick `tests` it ticks all the tests
+        # @todo: Set a icon for app image
+        #self.window.iconbitmap(os.path.dirname(os.path.realpath(__file__)) + "/favicon.ico")
+        # @todo: Find a way to loop through gif frames to have animated logo
+        logo = PhotoImage(file=os.path.dirname(os.path.realpath(__file__)) + "/logo.gif")
+        Label(self.window, image=logo).pack()
+        Button(self.window, text="Select Project Path", width=20, command=self.get_project_path).pack()
+        self.project = StringVar(self.window)
+        self.options = ['None', 'NULL']
+        self.project.trace("w", self.update_options)
+        dropdown = OptionMenu(self.window, self.project, *self.options_dict.keys())
+        dropdown.pack()
+        self.options_choices = Checkbar(self.window, self.options)
+        self.options_choices.pack(side=BOTTOM)
+        Button(self.window, text="Start", command=self.start).pack(side=BOTTOM)
+        Button(self.window, text="Exit", command=self.close_window).pack(side=BOTTOM)
+        self.window.bind('<Escape>', self.close_window_escape)
 
     def close_window(self):
         self.window.destroy()
@@ -39,47 +62,14 @@ class Application():
         self.path = filedialog.askdirectory()
         print(self.path)
 
-    def callback_project_name_changed(self, *args):
-        # @todo: Make groups like when you tick `tests` it ticks all the tests
-        self.project_selected = self.project.get()
-        if self.project_selected == "other":
-            # @todo: whitelist/blacklist for options
-            self.options = Checkbar(self.window, ['author', 'norme', 'Makefile'])
-            self.options.pack(side=LEFT)
-
-        if self.project_selected == "42commandements":
-            self.options = None
-
-        if self.project_selected == "libft":
-            self.options = Checkbar(self.window, ['author', 'forbidden-functions', 'makefile', 'norme', 'static', 'extra', 'required', 'bonus', 'benchmark', 'tests', 'libftest', 'maintest', 'moulitest', 'fillit-checker', 'libft-unit-test'])
-            self.options.pack(side=LEFT)
-
-        if self.project_selected == "fillit":
-            self.options = Checkbar(self.window, ['author', 'forbidden-functions', 'makefile', 'norme', 'tests', 'fillit-checker'])
-            self.options.pack(side=LEFT)
-
     def start(self):
-        if self.options != None:
-            print(list(self.options.state()))
+        print(list(self.options_choices.state()))
+
+    def update_options(self, *args):
+        self.options = self.options_dict[self.project.get()]
+        self.options_choices.destroy()
+        self.options_choices = Checkbar(self.window, self.options)
+        self.options_choices.pack(side=BOTTOM)
 
     def create_window(self):
-        self.window.title("42PyChecker")
-        self.window.minsize(width=800, height=800)
-        # @todo: Set a icon for app image
-        #self.window.iconbitmap(os.path.dirname(os.path.realpath(__file__)) + "/favicon.ico")
-        # @todo: Find a way to loop through gif frames to have animated logo
-        logo = PhotoImage(file=os.path.dirname(os.path.realpath(__file__)) + "/logo.gif")
-        Label(self.window, image=logo).pack()
-        Button(self.window, text="Select Project Path", width=20, command=self.get_project_path).pack()
-
-        self.project = StringVar(self.window)
-        dropdown = OptionMenu(self.window, self.project, "other", "42commandements", "libft", 'fillit')
-        dropdown.pack()
-
-        Button(self.window, text="Start", command=self.start).pack(side=BOTTOM)
-
-        Button(self.window, text="Exit", command=self.close_window).pack(side=BOTTOM)
-        self.window.bind('<Escape>', self.close_window_escape)
-
-        self.project.trace("w", self.callback_project_name_changed)
         self.window.mainloop()
